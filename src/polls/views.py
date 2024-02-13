5a007f2d94888878073911ce4d59f862b42e8e2c
Tor S. Haugland <tor.haugland@sintef.no>
2024-02-13 14:35:46 +0100

    Add azure authentication

--- src/polls/views.py
+++ src/polls/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.conf import settings
 
 from .models import Question, Choice
 
+try:
+    ms_identity_web = settings.MS_IDENTITY_WEB
+except:
+    class TempClass:
+        def login_required(self, f):
+            return f
+    ms_identity_web = TempClass()
 
+
+@ms_identity_web.login_required
 class IndexView(generic.ListView):
     template_name = "index.html"
     context_object_name = "latest_question_list"
@@ -14,16 +24,19 @@ class IndexView(generic.ListView):
         return Question.objects.all()
 
 
+@ms_identity_web.login_required
 class DetailView(generic.DetailView):
     model = Question
     template_name = "detail.html"
 
 
+@ms_identity_web.login_required
 class ResultView(generic.DetailView):
     model = Question
     template_name = "results.html"
 
 
+@ms_identity_web.login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     print(request.POST)
