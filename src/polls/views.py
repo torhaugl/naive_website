3d490b637ac6602fc229c2a6ba66b4770a90f2fa
Tor S. Haugland <tor.haugland@sintef.no>
2024-02-13 13:17:25 +0100

    Add polls

---
+++ src/polls/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return Question.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "detail.html"
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "results.html"
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    print(request.POST)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "detail.html",
+            {"question": question, "error_msg": "Selected choice does not exist"},
+        )
+
+    selected_choice.votes += 1
+    selected_choice.save()
+
+    return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
