a7ccfc146eaff6b401c7597f524aacc6386f89a9
Tor S. Haugland <tor.haugland@sintef.no>
2024-02-21 11:13:29 +0100

    Move templates and reduce authorization dependency

--- src/polls/views.py
+++ src/polls/views.py
@@ -2,42 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-from django.conf import settings
-from django.utils.decorators import method_decorator
 from physmet.PhysMetAppView import PhysMetAppView
 
 from .models import Question, Choice
 
-try:
-    ms_identity_web = settings.MS_IDENTITY_WEB
-except:
-    class TempClass:
-        def login_required(self, f):
-            return f
-    ms_identity_web = TempClass()
 
-
-@method_decorator(ms_identity_web.login_required, name="dispatch")
 class IndexView(generic.ListView):
-    template_name = "index.html"
+    template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
         return Question.objects.all()
 
 
-@method_decorator(ms_identity_web.login_required, name="dispatch")
 class DetailView(generic.DetailView):
     model = Question
-    template_name = "detail.html"
+    template_name = "polls/detail.html"
 
 
-#@method_decorator(ms_identity_web.login_required, name="dispatch")
-#class ResultView(generic.DetailView):
-#    model = Question
-#    template_name = "results.html"
-
-@ms_identity_web.login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     print(request.POST)
@@ -46,7 +28,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(
             request,
-            "detail.html",
+            "polls/detail.html",
             {"question": question, "error_msg": "Selected choice does not exist"},
         )
 
@@ -58,7 +40,7 @@ def vote(request, question_id):
 class ResultsView(PhysMetAppView):
     name = 'results'
     path = 'results'
-    template = 'results'
+    template = 'polls/results'
     login_required = True
     roles_required = {
         'get': 'portal-read | portal-admin',
